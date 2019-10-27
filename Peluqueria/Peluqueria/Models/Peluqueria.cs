561ef5bd82a6c0e66d90a758db99d50142ec43d2
Nelson De León <deleon213.ndl@gmail.com>
2019-10-27 18:17:47 -0300

    Initial commit

---
+++ Peluqueria/Peluqueria/Models/Peluqueria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peluqueria.Models
+{
+    public class Peluqueria
+
+    {
+
+        double CajaRegistradora;
+        int PrecioCorteDePelo;
+        int PrecioTintura;
+
+        public ICollection<Client> Clients{get; set;}
+
+    }
+}
