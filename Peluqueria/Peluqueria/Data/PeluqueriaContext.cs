5972133ea7e6db44ff4053d7e5be3e7949e92791
isilreiberth <isilreiberth@gmail.com>
2019-10-27 18:21:40 -0300

    First commit

---
+++ Peluqueria/Peluqueria/Data/PeluqueriaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Peluqueria.Model;
+
+namespace Peluqueria.Data
+{
+    public class PeluqueriaContext : DbContext
+    {
+        public PeluqueriaContext (DbContextOptions<PeluqueriaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ Peluqueria/Peluqueria/Model/Saldo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peluqueria.Model
+{
+    public class Saldo
+    {
+    }
+}
